bb18b5b2de2ea2cae7e9e343ea5c35db1303140a
Natan Macedo <natan.mac.n@gmail.com>
2025-06-23 14:05:03 -0300

    :tada: Adicionado rotas + axios + cadastro de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/footer';
 import Navbar from './components/navbar/navbar';
 import Home from './pages/home/Home';
+import Cadastro from './pages/cadastro/cadastro';
+import Login from './pages/login/login';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
